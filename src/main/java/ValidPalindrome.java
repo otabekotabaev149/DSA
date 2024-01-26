fbcab1b0576d8871c014dae72caec38c50bcb621
JohnBryan2822 <johnbryan2822@gmail.com>
2024-01-26 23:21:16 +0100

    Valid Palindrome - DSA

--- src/main/java/ValidPalindrome.java
+++ src/main/java/ValidPalindrome.java
@@ -1,2 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static boolean isPalindrome(String s){
+        s = s.toLowerCase();
+        Pattern pattern = Pattern.compile("[a-z0-9]");
+        Matcher matcher = pattern.matcher(s);
+        String str = "";
+        while(matcher.find()){
+            str += matcher.group();
+        }
+        if(str.equals(new String(new StringBuilder(str).reverse()))){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
