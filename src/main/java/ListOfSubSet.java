99e018881c582ead332e2b572e43432078e77566
JohnBryan2822 <johnbryan2822@gmail.com>
2024-01-28 20:37:39 +0100

    List of all subsets in array - DSA

---
+++ src/main/java/ListOfSubSet.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfSubSet {
+    public static void main(String[] args) {
+//        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
+    }
+
+    public static List<List<Integer>> subsets(int[] arr) {
+        List<List<Integer>> list = new ArrayList<>();
+        int len = arr.length;
+        for(int i=0;i< (1<<len);i++){
+            List<Integer> l = new ArrayList<>();
+            for(int j=0;j<len;j++){
+                if((i & (1<<j)) != 0){
+                    l.add(arr[j]);
+                }
+            }
+            list.add(l);
+        }
+        return list;
+    }
+}
