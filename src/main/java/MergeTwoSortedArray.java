f3c8e7fa766631c6aa21355ccff1f4035cd65511
JohnBryan2822 <johnbryan2822@gmail.com>
2024-07-12 12:58:22 +0200

    MergeTwoSortedArray updated - DSA

--- src/main/java/MergeTwoSortedArray.java
+++ src/main/java/MergeTwoSortedArray.java
@@ -5,6 +5,30 @@ public class MergeTwoSortedArray {
         merge(arr1, 3, arr2, 3);
     }
 
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int len = m + n - 1; // Pointer for the last position in nums1
+        m--; // Pointer for the last element in the initial part of nums1
+        n--; // Pointer for the last element in nums2
+
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] >= nums2[n]) {
+                nums1[len] = nums1[m];
+                m--;
+            } else {
+                nums1[len] = nums2[n];
+                n--;
+            }
+            len--;
+        }
+
+        // If there are any remaining elements in nums2, copy them
+        while (n >= 0) {
+            nums1[len] = nums2[n];
+            len--;
+            n--;
+        }
+    }
+
     private static void merge(int[] arr1, int m, int[] arr2, int n) {
         int len = arr1.length-1;
         m--;
@@ -29,9 +53,6 @@ public class MergeTwoSortedArray {
             arr1[len] = arr2[n];
             len--;
             n--;
-        }
-        for(int lk : arr1){
-            System.out.print(lk + " ");
         }
     }
 }
