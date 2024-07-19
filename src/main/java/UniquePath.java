d0af89a53706a4ab732b62ac85e16f2a86ea5044
JohnBryan2822 <johnbryan2822@gmail.com>
2024-07-19 16:04:28 +0200

    Grid unique paths - DSA

--- src/main/java/UniquePath.java
+++ src/main/java/UniquePath.java
@@ -3,6 +3,28 @@ public class UniquePath {
 
     }
 
+    private static int countPaths(int i, int j, int n, int m, int[][] matrix){
+        if(i == (n-1) && j == (m-1)) return 1;
+        if(i >= n || j >= m) return 0;
+
+        if(matrix[i][j] != -1) return matrix[i][j];
+
+        matrix[i][j] = countPaths(i+1, j, n, m, matrix) + countPaths(i, j+1, n, m, matrix);
+        return matrix[i][j];
+    }
+
+    private int uniquePaths2(int m, int n) {
+        int[][] matrix = new int[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = -1;
+            }
+        }
+
+        return countPaths(0, 0, n, m, matrix);
+    }
+
     private int uniquePaths(int m, int n) {
         int N = n + m - 2;
         int r = m - 1;
